b66e08da9f284074a41959f27d139a064a01a0f4
idksarah <sarah.p.ngai@gmail.com>
2025-10-06 23:01:00 -0700

    capture photos

--- src/pages/Photobooth.tsx
+++ src/pages/Photobooth.tsx
@@ -3,6 +3,9 @@ import photobooth from "./../assets/photobooth.png";
 
 type StatusType = "idle" | "recording" | "paused";
 
+var photos: string[] = [];
+var chunks = [];
+
 export default function Photobooth() {
     const [status, setStatus] = useState<StatusType>("idle");
     const [recorder, setRecorder] = useState<MediaRecorder|null>(null);
@@ -11,12 +14,14 @@ export default function Photobooth() {
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const photoRef = useRef<HTMLImageElement>(null);
+    const mediaRecorderRef = useRef<MediaRecorder|null>(null);
 
     useEffect(() => {
-        if(status==="recording" && stream && videoRef.current) {
+        if(status === "recording" && stream && videoRef.current) {
             videoRef.current.srcObject = stream
         }
     }, [status, videoRef.current, status])
+
     const startRecording = async () => {
         try {
             const stream = await navigator.mediaDevices.getUserMedia({
@@ -24,22 +29,48 @@ export default function Photobooth() {
                 audio: false
             })
             setStream(stream);
-            const mediaRecorder = new MediaRecorder(stream);
-            setRecorder(mediaRecorder);
 
-            mediaRecorder.onstart = () => {
+            mediaRecorderRef.current = new MediaRecorder(stream);
+            mediaRecorderRef.current.ondataavailable = (e) => {
+                chunks.push(e.data);
+            };
+
+            setRecorder(mediaRecorderRef.current);
+
+            mediaRecorderRef.current.onstart = () => {
                 if(videoRef.current) videoRef.current.play();
             }
-            
-            mediaRecorder.onstop = () => {
+
+            mediaRecorderRef.current.onstop = () => {
+
                 if(videoRef.current) videoRef.current.pause();
+                mediaRecorderRef.current = null;
             }
-            
-            mediaRecorder.start();
+
+            mediaRecorderRef.current.start();
             setStatus("recording");
         } catch (err) {
             console.error("error:", err);
         }
+    }
+
+    const capture = async () => {
+        if (mediaRecorderRef.current && canvasRef.current && videoRef.current) {
+            canvasRef.current.width = videoRef.current.videoWidth;
+            canvasRef.current.height = videoRef.current.videoHeight;
+            const context = canvasRef.current.getContext("2d");
+            if (!context) return
+            context.drawImage(videoRef.current, 0, 0, 
+            videoRef.current.videoWidth, videoRef.current.videoHeight);
+
+            canvasRef.current.toBlob((blob: Blob | null) => {
+                if (blob) {
+                    const img = new Image();
+                    img.src = window.URL.createObjectURL(blob);
+                    photos.push(img.src);
+                }
+            });
+        } else console.log("media recorder is null");
     }
 
     const stopRecording = () => {
@@ -59,13 +90,11 @@ export default function Photobooth() {
         <section>
             <div className="camera">
                 <video id="video" ref={videoRef}>video stream not available</video>
-                <button id="captureButton" onClick={(recorder) ? stopRecording : startRecording}>
-                    take pic loser</button>
+                <button id="startButton" onClick={(recorder) ? stopRecording : startRecording}>
+                    start loser</button>
+                    <button id="captureButton" onClick={capture}>take pic loser</button>
             </div>
             <canvas id="canvas" ref={canvasRef}></canvas>
-            <div className="output">
-                <img id="photo" alt="The screen capture will appear in this box." ref={photoRef}/>
-            </div>
         </section>
         </section>
         </div>
